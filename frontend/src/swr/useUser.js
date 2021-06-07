bf0dc8614a8ba35b10837cc95ee01f8552975846
ramdhanll <erdevlop@gmail.com>
2021-06-07 23:38:49 +0700

    created useUser hook SWR

--- frontend/src/swr/useUser.js
+++ frontend/src/swr/useUser.js
@@ -1,43 +1,22 @@
 import axios from 'axios'
 import useSWR from 'swr'
 
-// const fetcher = async () =>
-
-//    await fetch({
-//       method: 'GET',
-//       headers: {
-//          latitude: '34.211822',
-//          longitude: '-118.646435',
-//       },
-//    }).then((response) => response.json())
-
-// const useUser = (id) => {
-//    const { data, error } = useSWR(
-//       `${process.env.REACT_APP_BASE_URL_SERVER}/api/users/${id}`
-//    )
-
-//    return {
-//       user: data,
-//       isLoading: !error & !data,
-//       isError: error,
-//    }
-// }
-
 const useUser = (id, token) => {
-   const fetcher = (url, token) => {
+   const fetcher = (url, token) =>
       axios
-         .get(url, { headers: { Authorization: 'Bearer ' + token } })
+         .get(url, {
+            headers: { Authorization: `Bearer ${token}` },
+         })
          .then((res) => res.data)
-   }
-   const { data, error } = useSWR(
-      [`${process.env.REACT_APP_BASE_URL_SERVER}/api/users/${id}`, token],
-      fetcher
-   )
+
+   const { data, error } = useSWR([`api/users/${id}`, token], fetcher, {
+      revalidateOnFocus: false,
+   })
 
    return {
-      data,
-      error,
+      user: data,
+      isLoading: !error && !data,
+      isError: error,
    }
 }
-
 export default useUser
